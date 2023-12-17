c3e799c9ae391de9f428be2349cf9e48eec30ada
Crowdbotics <team@crowdbotics.com>
2023-12-17 00:35:25 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "yevalo-45059",
+  url: "https://yevalo-45059.botics.co",
+  api: "https://yevalo-45059.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
